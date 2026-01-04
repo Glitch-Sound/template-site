0340107642972890b1a6de46f58c66c8c547e2b2
Yuichi Satoh <uid.glitch@gmail.com>
2026-01-04 17:19:23 +0900

    update.

--- frontend/app/src/stores/ChartFilterStore.ts
+++ frontend/app/src/stores/ChartFilterStore.ts
@@ -4,7 +4,7 @@ import { defineStore } from 'pinia'
 import { TypeRank } from '@/types/Project'
 
 export const useChartFilterStore = defineStore('chartFilter', () => {
-  const selectedRanks = ref<TypeRank[]>([TypeRank.A])
+  const selectedRanks = ref<TypeRank[]>([TypeRank.A, TypeRank.B])
   const amountMode = ref<'order' | 'expected'>('order')
 
   return {
