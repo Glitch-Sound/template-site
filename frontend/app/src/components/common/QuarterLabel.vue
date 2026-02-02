f3c871346aeb2d54d16a27de8ed7a315d3991e0e
Yuichi Satoh <uid.glitch@gmail.com>
2026-02-03 08:41:51 +0900

    update.

--- frontend/app/src/components/common/QuarterLabel.vue
+++ frontend/app/src/components/common/QuarterLabel.vue
@@ -6,24 +6,23 @@ const props = defineProps<{
   project: Project
 }>()
 
-const year = computed(() => {
-  return Math.floor(props.project.target_quarter / 10)
-})
+const year = computed(() => Math.floor(props.project.target_quarter / 10))
 
-const quarter = computed(() => {
-  return `${props.project.target_quarter % 10}Q`
-})
+const quarter = computed(() => props.project.target_quarter % 10)
+
+const displayYear = computed(() => (year.value === 0 ? 'XXXX' : String(year.value)))
+const displayQuarter = computed(() => (quarter.value === 0 ? '' : `${quarter.value}Q`))
 </script>
 
 <template>
   <v-row dense class="ma-0 pa-0 flex-column align-center">
     <v-col cols="auto" class="pa-0 text-center">
       <span class="text-subtitle-1 font-weight-black gray-light">
-        {{ year }}
+        {{ displayYear }}
       </span>
     </v-col>
     <v-col cols="auto" class="pa-0 text-center gray-dark mt-n1">
-      {{ quarter }}
+      {{ displayQuarter }}
     </v-col>
   </v-row>
 </template>
